ddecbd1612854143cc2756416afbd6da46b2b4f0
Likeamarc <72190617+Likeamarc@users.noreply.github.com>
2021-03-29 19:09:56 +0200

    register

---
+++ app/src/main/java/com/example/practicapis/registerActivity.java
@@ -0,0 +1,13 @@
+package com.example.practicapis;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toolbar;
+
+public class registerActivity{
+
+}
+
+

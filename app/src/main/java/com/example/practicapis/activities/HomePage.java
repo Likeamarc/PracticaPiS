b5275c075837945eb925f0de564af69fbdc7ecc9
Likeamarc <72190617+Likeamarc@users.noreply.github.com>
2021-04-16 17:32:06 +0200

    unos arreglos y el inicio de la Database, Marc

--- app/src/main/java/com/example/practicapis/activities/HomePage.java
+++ app/src/main/java/com/example/practicapis/activities/HomePage.java
@@ -1,19 +1,24 @@
 package com.example.practicapis.activities;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.example.practicapis.Adapter;
 import com.example.practicapis.ExampleItem;
 import com.example.practicapis.R;
+import com.example.practicapis.database.NoteDatabase;
+import com.example.practicapis.entities.Note;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 
 public class HomePage extends AppCompatActivity {
@@ -39,6 +44,8 @@ public class HomePage extends AppCompatActivity {
             }
         });
 
+        getNotes();
+
         ArrayList<ExampleItem> exampleList = new ArrayList<>();
         exampleList.add(new ExampleItem(R.drawable.ic_baseline_camera_alt_24, "Camera Note", "Description"));
         exampleList.add(new ExampleItem(R.drawable.ic_baseline_speaker_24, "Sound Note", "Description"));
@@ -51,8 +58,22 @@ public class HomePage extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
 
-
-
     }
 
+    private void getNotes(){
+
+        class getNotesText extends AsyncTask<Void, Void, List<Note>>{
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NoteDatabase.getDatabase(getApplicationContext()).noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                Log.d("My Notes", notes.toString());
+            }
+        }
+        new getNotesText().execute();
+    }
 }
--- app/src/main/java/com/example/practicapis/activities/new_note.java
+++ app/src/main/java/com/example/practicapis/activities/new_note.java
@@ -1,24 +1,83 @@
 package com.example.practicapis.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.practicapis.R;
+import com.example.practicapis.database.NoteDatabase;
+import com.example.practicapis.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class new_note extends AppCompatActivity {
 
+    private EditText inputTitle, inputText;
+    private TextView dateTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        setContentView(R.layout.new_note);
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_note);
 
         ImageView imageBack = findViewById(R.id.imageBack);
         imageBack.setOnClickListener(v -> onBackPressed());
+
+        inputTitle = findViewById(R.id.inputNoteTitle);
+        inputText = findViewById(R.id.inputNote);
+        dateTime = findViewById(R.id.textDataTime);
+
+        dateTime.setText(
+                new SimpleDateFormat("EEE dd MMMM yyy HH:mm a", Locale.getDefault())
+                        .format(new Date())
+        );
+
+        ImageView imageDone = findViewById(R.id.imageSave);
+        imageDone.setOnClickListener(v -> saveNote());
+    }
+
+    private void saveNote(){
+        if(inputTitle.getText().toString().trim().isEmpty()){
+            Toast.makeText(this, "Note title can't be empty", Toast.LENGTH_LONG).show();
+            return;
+        }else if (inputText.getText().toString().trim().isEmpty()){
+            Toast.makeText(this, "Note can't be empty", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        final Note note = new Note();
+        note.setTitle(inputTitle.getText().toString());
+        note.setNoteText(inputText.getText().toString());
+
+
+        @SuppressLint("StaticFieldLeak")
+        class saveNoteTask extends AsyncTask<Void, Void, Void>{
+
+            @Override
+            protected Void doInBackground(Void... voids){
+                NoteDatabase.getDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+
+        new saveNoteTask().execute();
     }
 }

4e7434cdeb6d5f4d5c1dd40b1c5c36f3102e9274
Fabian <nekunessdw@gmail.com>
2021-05-15 17:46:19 +0200

    Subo commit con el login conectado a database, menu de settings, loginviewmodel desactivado temporalmente mientras miro como hacer que funcione con la pantalla de login usando la database

--- app/src/main/java/com/example/practicapis/entities/Login.java
+++ app/src/main/java/com/example/practicapis/entities/Login.java
@@ -1,42 +1,67 @@
 package com.example.practicapis.entities;
 
 import androidx.annotation.Nullable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
-public class Login {
+import java.io.Serializable;
 
-    @Nullable
-    String username, email, password;
+@Entity(tableName = "users")
+public class Login implements Serializable {
 
-    public Login(String username, String email, String password){
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+    @PrimaryKey
+    String username;
 
-    @Nullable
+    @ColumnInfo(name = "email")
+    String email;
+
+    @ColumnInfo(name = "password")
+    String password;
+
+    @ColumnInfo(name = "name")
+    String name;
+
+    @ColumnInfo(name = "lastname")
+    String lastname;
+
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(@Nullable String username) {
+    public void setUsername(String username) {
         this.username = username;
     }
 
-    @Nullable
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(@Nullable String email) {
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    @Nullable
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(@Nullable String password) {
+    public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 }
